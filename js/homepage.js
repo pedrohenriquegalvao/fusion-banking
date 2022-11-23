11e3da0a92db27b2041885019cc6370bc78061cb
Pedro Henrique Galvão <70819419+pedrohenriquegalvao@users.noreply.github.com>
2022-11-23 12:05:37 -0300

    Adicionando links aos cards de atalho

---
+++ js/homepage.js
@@ -0,0 +1,9 @@
+let cards = document.getElementsByClassName("card-atalho");
+
+cards[1].addEventListener("click", () => {
+    window.location.href = "/pages/loan.html"
+});
+
+cards[2].addEventListener("click", () => {
+    window.location.href = "/pages/payment.html"
+});
