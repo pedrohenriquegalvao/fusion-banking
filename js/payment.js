0bf721a2f7a7923af83959ac26133133795ad432
Pedro Henrique Galvão <70819419+pedrohenriquegalvao@users.noreply.github.com>
2022-11-28 23:32:46 -0300

    Prosseguindo com a lógica página de pagamento de boleto.

---
+++ js/payment.js
@@ -0,0 +1,82 @@
+let buttonStep1 = document.getElementById('btn-cont1');
+
+let containerPasso1 = document.getElementsByClassName('container-passo1');
+
+let hoje = () => {
+    let hoje = new Date();
+    let dia = hoje.getDate();
+    let mes = hoje.getMonth() + 1;
+    let ano = hoje.getFullYear();
+    return `${dia}/${mes}/${ano}`;
+}
+
+class Boleto {
+    constructor(codigoBarras, valor, nome, data) {
+        this._codigoBarras = codigoBarras;
+        this._valor = valor;
+        this._nome = nome;
+        this._data = data;
+    }
+    
+    get codigoBarras() {
+        return this._codigoBarras;
+    }
+
+    get valor() {
+        return this._valor;
+    }
+
+    get nome() { 
+        return this._nome;
+    }
+
+    get data() {
+        return this._data;
+    }
+}
+
+let valorBoleto = Math.floor(Math.random() * (400 - 70 + 1) + 70);
+
+buttonStep1.addEventListener('click', (e) => {
+    e.preventDefault();
+    resetStyles();
+    const codigo1 = document.getElementById('codigo1').value;
+    const codigo2 = document.getElementById('codigo2').value;
+    const codigo3 = document.getElementById('codigo3').value;
+    const codigo4 = document.getElementById('codigo4').value;
+ 
+    const nomeBoleto = document.getElementById('nome-pagamento').value;
+
+    if(!/^[0-9]+$/.test(codigo1) || !/^[0-9]+$/.test(codigo2) || !/^[0-9]+$/.test(codigo3) || !/^[0-9]+$/.test(codigo4)) {
+        document.getElementById('span-codigo-barras').style.display = "block";
+    }
+    if (nomeBoleto === ""){
+        document.getElementById('span-nome-boleto').style.display = "block";
+    } 
+    else {
+        let codigoBarras = codigo1 + " " + codigo2 + " " + codigo3 + " " + codigo4;
+        let valor = valorBoleto;
+        let nome = nomeBoleto;
+
+        let boleto = new Boleto(codigoBarras, valor, nome, hoje());
+        
+        
+        document.getElementById('valor-boleto').innerHTML = `R$${valor}`
+        
+
+        // Colocando os valores na tabela do passo 2
+        document.getElementById('codigo-barras').innerHTML = boleto.codigoBarras;
+        document.getElementById('data').innerHTML = boleto.data;
+        document.getElementById('valor').innerHTML = `R$${boleto.valor},00`;
+        document.getElementById('identificacao').innerHTML = boleto.nome;
+    }
+
+    
+     
+    
+    
+});
+
+function resetStyles() {
+    document.getElementById('span-codigo-barras').style.display = "none";
+}
